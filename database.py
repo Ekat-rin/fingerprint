dcd4e22b6fb1bfa04cec904d26d319f88d370625
k.p97.ekat <k.p97.ekat@gmail.com>
2020-11-08 09:45:31 +0300

    add database class

---
+++ database.py
@@ -0,0 +1,32 @@
+import psycopg2
+import json
+
+
+class DataBase:
+    def __init__(self):
+        with open("database_config.json", "r") as read_file:
+            database_conf = json.load(read_file)
+        self.conn = psycopg2.connect(database=database_conf["database"],
+                                     user=database_conf["user"],
+                                     password=database_conf["password"],
+                                     host=database_conf["host"],
+                                     port=database_conf["port"])
+        self.cur = self.conn.cursor()
+
+    def get_last_time_from_last_online_time(self, id_user):
+        self.cur.execute(
+            """SELECT user_id, last_online_time FROM user_last_online 
+                        WHERE user_id = '""" + id_user + """'""")
+        return self.cur.fetchall()
+
+    def update_last_online_time(self, id_user, new_time):
+        self.cur.execute("""UPDATE user_last_online set last_online_time
+                                          = '""" + new_time + """' where 
+                                      user_id ='""" + id_user + """'""")
+        self.conn.commit()
+
+        #добавить работу базы с fingerprint
+
+    def __del__(self):
+        self.cur.close()
+        self.conn.close()
